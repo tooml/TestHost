e22f70f338d2a500a479a0585aa6aeb584828d6b
tooml <toom-l@hotmail.de>
2015-05-05 12:36:56 +0200

    Service
    Host-Service

---
+++ HostFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHost
+{
+
+        [ServiceContract]
+        public class HostFunctions
+        {
+            [OperationContract]
+            public string getDate()
+            {
+                return DateTime.Today.DayOfWeek.ToString();
+            }
+
+            [OperationContract]
+            public string getTim()
+            {
+                return DateTime.Now.ToString();
+            }
+        }
+    
+}
